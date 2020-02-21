7dd96cc5d9dc09af675a98b6bcaa7a671d2e4062
Jordan Parsons <49204159+jordanparsons9925@users.noreply.github.com>
2020-02-21 00:11:32 -0330

    made improvements to interface

--- scripts/controls.js
+++ scripts/controls.js
@@ -5,26 +5,28 @@ var scrollingBackground = {
 }
 var canvas = document.getElementById("screen");
 var screen = canvas.getContext("2d");
+var movementCounter = 0;
 
 var renderBackground = function() {
     screen.strokeStyle = "#FFFFFF";
     screen.fillRect(0, 0, 1280, 720);
     screen.drawImage(scrollingBackground.Image, scrollingBackground.X, scrollingBackground.Y);
-    screen.drawImage(scrollingBackground.Image, scrollingBackground.X+1280, scrollingBackground.Y);
-    screen.drawImage(scrollingBackground.Image, scrollingBackground.X, scrollingBackground.Y+720);
-    screen.drawImage(scrollingBackground.Image, scrollingBackground.X+1280, scrollingBackground.Y+720);
+    screen.drawImage(scrollingBackground.Image, scrollingBackground.X+1279, scrollingBackground.Y);
+    screen.drawImage(scrollingBackground.Image, scrollingBackground.X, scrollingBackground.Y+719);
+    screen.drawImage(scrollingBackground.Image, scrollingBackground.X+1279, scrollingBackground.Y+719);
     scrollingBackground.X -= 1;
-    scrollingBackground.Y -= 0.5925;
-    if (scrollingBackground.X == 2560 && scrollingBackground.Y == 1440) {
+    scrollingBackground.Y -= 0.5629;
+    if (movementCounter++ == 1280) {
         scrollingBackground.X = 0;
         scrollingBackground.Y = 0;
+        movementCounter = 0;
     }
         
     
 }
 
 window.onload = function() {
-    screen.width = 1280;
-    screen.height = 720;
+    screen.width = document.getElementById("screen").style.width / 2;
+    screen.height = document.getElementById("screen").style.height / 2;
     window.setInterval(renderBackground, 16);
 }
