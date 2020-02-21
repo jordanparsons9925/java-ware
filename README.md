575ed47fa19f60d05d13d722f72833f09fbcbbc2
Jordan Parsons <49204159+jordanparsons9925@users.noreply.github.com>
2020-02-20 20:35:47 -0330

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# java-ware
+ javascript minigames
---
+++ controls.js
@@ -0,0 +1,6 @@
+var canvas = document.getElementById("screen");
+var screen = canvas.getContext("2d");
+screen.width = 1280;
+screen.height = 720;
+screen.strokeStyle = "#000000";
+screen.fillRect(20, 20, 50, 50);
